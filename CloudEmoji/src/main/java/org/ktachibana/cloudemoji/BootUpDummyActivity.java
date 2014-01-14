516f48350c931b3d561c5f16c1ddfa9610fa37a5
KTachibanaM <whj19931115@gmail.com>
2014-01-14 15:24:57 -0500

    Make boot up dummy activity use notification helper

--- CloudEmoji/src/main/java/org/ktachibana/cloudemoji/BootUpDummyActivity.java
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/BootUpDummyActivity.java
@@ -3,17 +3,19 @@ package org.ktachibana.cloudemoji;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 /**
- * Fires notification after boot up if user preference said so
+ * Shows notification after boot up according to user preference
  */
 public class BootUpDummyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Boolean test = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(SettingsActivity.PREF_SHOW_AFTER_BOOT_UP, true);
-        Log.e("233", Boolean.toString(test));
+        Boolean showAfterBootUp = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(SettingsActivity.PREF_SHOW_AFTER_BOOT_UP, true);
+        if (showAfterBootUp) {
+            String notificationVisibility = PreferenceManager.getDefaultSharedPreferences(this).getString(SettingsActivity.PREF_NOTIFICATION_VISIBILITY, "both");
+            NotificationHelper.switchNotificationState(this, notificationVisibility);
+        }
         finish();
     }
 }
