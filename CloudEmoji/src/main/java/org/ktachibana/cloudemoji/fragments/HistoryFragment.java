4a7f69ea131f817ac11d0ec9c6164405ebedf923
KTachibanaM <whj19931115@gmail.com>
2014-06-30 01:16:09 +0800

    Implement remove all history

--- CloudEmoji/src/main/java/org/ktachibana/cloudemoji/fragments/HistoryFragment.java
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/fragments/HistoryFragment.java
@@ -3,6 +3,9 @@ package org.ktachibana.cloudemoji.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -36,6 +39,29 @@ public class HistoryFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceBundle) {
+        super.onCreate(savedInstanceBundle);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_history, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_remove_history: {
+                History.deleteAll(History.class);
+                mAdapter.updateHistory();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,6 +86,4 @@ public class HistoryFragment extends Fragment {
         });
         return rootView;
     }
-
-
 }
