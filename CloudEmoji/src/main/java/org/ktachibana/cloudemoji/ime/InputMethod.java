ed58b0607f462bc225495af865ce27c16080ed10
KTachibanaM <whj19931115@gmail.com>
2014-12-27 20:07:14 +0800

    IME

---
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/ime/InputMethod.java
@@ -0,0 +1,46 @@
+package org.ktachibana.cloudemoji.ime;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.KeyboardView;
+
+public class InputMethod extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
