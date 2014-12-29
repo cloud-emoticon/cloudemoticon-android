69af9a22388beea39714fd80c3df9298aafc2200
KTachibanaM <whj19931115@gmail.com>
2014-12-29 09:58:00 +0800

    Successfull IME trial

--- CloudEmoji/src/main/java/org/ktachibana/cloudemoji/ime/InputMethod.java
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/ime/InputMethod.java
@@ -1,11 +1,18 @@
 package org.ktachibana.cloudemoji.ime;
 
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
+import org.ktachibana.cloudemoji.R;
 
 public class InputMethod extends InputMethodService {
 
     private InputMethodManager mManager;
+    private Keyboard mKeyboard;
+    private KeyboardView mKeyboardView;
 
     @Override
     public void onCreate() {
@@ -16,5 +23,14 @@ public class InputMethod extends InputMethodService {
     @Override
     public void onInitializeInterface() {
         super.onInitializeInterface();
+
+        mKeyboard = new Keyboard(this, R.xml.ime_layout_template, "qwertyuiop", 5, 1);
+    }
+
+    @Override
+    public View onCreateInputView() {
+        mKeyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.view_ime, null);
+        mKeyboardView.setKeyboard(mKeyboard);
+        return mKeyboardView;
     }
 }
