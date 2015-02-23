86843c6e7e733bcf18abc88021e84f7c62eae78f
KTachibanaM <whj19931115@gmail.com>
2015-02-23 10:49:47 -0500

    Revert to ee5cf1c

--- CloudEmoji/src/main/java/org/ktachibana/cloudemoji/activities/MainActivity.java
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/activities/MainActivity.java
@@ -89,8 +89,8 @@ public class MainActivity extends BaseActivity implements
     // Views
     @InjectView(R.id.drawerLayout)
     DrawerLayout mDrawerLayout;
-    @InjectView(R.id.primaryToolbar)
-    Toolbar primaryToolbar;
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
     private ActionBarDrawerToggle toggle;
     // State
     private long mCurrentRepositoryId;
@@ -160,7 +160,7 @@ public class MainActivity extends BaseActivity implements
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
-        setSupportActionBar(primaryToolbar);
+        setSupportActionBar(toolbar);
         toggle = new ActionBarDrawerToggle(
                 this,
                 mDrawerLayout,
