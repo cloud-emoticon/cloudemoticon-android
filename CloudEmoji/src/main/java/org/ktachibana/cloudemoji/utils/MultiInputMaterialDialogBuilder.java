c468661cd7ace39042d461977c1d5ee9ce452d56
KTachibanaM <whj19931115@gmail.com>
2015-04-26 09:10:15 -0400

    Refine MultiInputMaterialDialogBuilder

--- CloudEmoji/src/main/java/org/ktachibana/cloudemoji/utils/MultiInputMaterialDialogBuilder.java
+++ CloudEmoji/src/main/java/org/ktachibana/cloudemoji/utils/MultiInputMaterialDialogBuilder.java
@@ -8,43 +8,56 @@ import android.widget.LinearLayout;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MultiInputMaterialDialogBuilder extends MaterialDialog.Builder {
     private Context mContext;
+    private List<EditText> mEditTexts;
     private LinearLayout mRootView;
-    private InputsCallback mCallback;
 
     public MultiInputMaterialDialogBuilder(@NonNull Context context) {
         super(context);
         mContext = context;
+        mEditTexts = new ArrayList<>();
 
         mRootView = new LinearLayout(context);
         mRootView.setOrientation(LinearLayout.VERTICAL);
     }
 
-    public MultiInputMaterialDialogBuilder addInput(CharSequence title, CharSequence preFill, CharSequence hint, int type) {
+    public MultiInputMaterialDialogBuilder addInput(CharSequence preFill, CharSequence hint) {
         EditText newEditText = new EditText(mContext);
         newEditText.setText(preFill);
         newEditText.setHint(hint);
 
+        mEditTexts.add(newEditText);
         mRootView.addView(newEditText);
+
         this.customView(mRootView, true);
         return this;
     }
 
-    public MultiInputMaterialDialogBuilder addInput(@StringRes int title, @StringRes int preFill, @StringRes int hint, int type) {
+    public MultiInputMaterialDialogBuilder addInput(@StringRes int preFill, @StringRes int hint) {
         return addInput(
-                title == 0 ? null : mContext.getString(title),
                 preFill == 0 ? null : mContext.getString(preFill),
-                hint == 0 ? null : mContext.getString(hint),
-                type
+                hint == 0 ? null : mContext.getString(hint)
         );
     }
 
-    public MultiInputMaterialDialogBuilder inputs(@NonNull InputsCallback callback) {
-        mCallback = callback;
+    public MultiInputMaterialDialogBuilder inputs(@NonNull final InputsCallback callback) {
+        this.callback(new MaterialDialog.ButtonCallback() {
+            @Override
+            public void onPositive(MaterialDialog dialog) {
+                super.onPositive(dialog);
+
+                List<CharSequence> inputs = new ArrayList<>();
+                for (EditText editText : mEditTexts) {
+                    inputs.add(editText.getText());
+                }
+                callback.onInput(dialog, inputs);
+            }
+        });
         return this;
     }
 
