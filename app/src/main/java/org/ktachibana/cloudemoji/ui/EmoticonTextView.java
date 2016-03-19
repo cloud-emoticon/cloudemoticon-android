3e2149eea41fef5714a2db34ffde96184136f8bf
KTachibanaM <whj19931115@gmail.com>
2016-03-19 12:09:58 -0500

    etv

--- app/src/main/java/org/ktachibana/cloudemoji/ui/EmoticonTextView.java
+++ app/src/main/java/org/ktachibana/cloudemoji/ui/EmoticonTextView.java
@@ -5,12 +5,27 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class EmoticonTextView extends TextView {
+    private int width = -1;
+    private int height = -1;
+    private boolean sizeObtained = false;
+
     public EmoticonTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = w;
+        height = h;
+        sizeObtained = true;
+    }
+
+    @Override
     public void setText(CharSequence text, BufferType type) {
         super.setText(text, type);
+        if (sizeObtained) {
+
+        }
     }
 }
