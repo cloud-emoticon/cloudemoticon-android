cc7db5b4bcd61471b379e0fc2f03bd2ddbeb81ea
KTachibanaM <whj19931115@gmail.com>
2016-06-28 01:02:12 -0700

    fix scroll && list for bookmarks

--- app/src/main/java/org/ktachibana/cloudemoji/utils/EmoticonHeadService.java
+++ app/src/main/java/org/ktachibana/cloudemoji/utils/EmoticonHeadService.java
@@ -1,6 +1,7 @@
 package org.ktachibana.cloudemoji.utils;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Handler;
@@ -13,12 +14,25 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.ktachibana.cloudemoji.R;
+import org.ktachibana.cloudemoji.adapters.FavoriteListViewAdapter;
+import org.ktachibana.cloudemoji.models.disk.Favorite;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,11 +40,15 @@ import butterknife.ButterKnife;
 public class EmoticonHeadService extends Service {
     private WindowManager windowManager;
     private View rootView;
+
     private boolean showing;
     @Bind(R.id.icon)
     ImageView icon;
-    @Bind(R.id.window)
-    FrameLayout window;
+
+    List<Favorite> favorites;
+    Adapter adapter;
+    @Bind(R.id.list)
+    ListView list;
 
     @Nullable
     @Override
@@ -59,18 +77,38 @@ public class EmoticonHeadService extends Service {
         params.x = 0;
         params.y = 100;
 
+        // Add
+        windowManager.addView(rootView, params);
+
+        rootView.post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
         // Gesture detector for icon
-        final GestureDetectorCompat detector = new GestureDetectorCompat(this, new GestureDetector.SimpleOnGestureListener() {
+        final GestureDetectorCompat detector = new GestureDetectorCompat(EmoticonHeadService.this, new GestureDetector.SimpleOnGestureListener() {
+            private float diffX;
+            private float diffY;
+
             @Override
             public boolean onDown(MotionEvent e) {
+                diffX = e.getRawX() - params.x;
+                diffY = e.getRawY() - params.y;
                 return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                // todo: refresh
             }
 
             @Override
             public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
                 // When scrolled, move the whole view
-                params.x = (int) e2.getRawX();
-                params.y = (int) e2.getRawY();
+                params.x = (int) (e2.getRawX() - diffX);
+                params.y = (int) (e2.getRawY() - diffY);
                 windowManager.updateViewLayout(rootView, params);
                 return true;
             }
@@ -79,9 +117,9 @@ public class EmoticonHeadService extends Service {
             public boolean onSingleTapConfirmed(MotionEvent e) {
                 // When clicked, show/hide window
                 if (showing) {
-                    window.setVisibility(View.GONE);
+                    list.setVisibility(View.GONE);
                 } else {
-                    window.setVisibility(View.VISIBLE);
+                    list.setVisibility(View.VISIBLE);
                 }
                 showing = !showing;
                 return true;
@@ -95,8 +133,16 @@ public class EmoticonHeadService extends Service {
             }
         });
 
-        // Add
-        windowManager.addView(rootView, params);
+        // List
+        favorites = Favorite.listAll(Favorite.class);
+        adapter = new Adapter(this, favorites);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                CopyUtils.copyToClipboard(EmoticonHeadService.this, favorites.get(position).getEmoticon());
+            }
+        });
     }
 
     @Override
@@ -104,6 +150,27 @@ public class EmoticonHeadService extends Service {
         super.onDestroy();
         if (rootView != null) {
             windowManager.removeView(rootView);
+        }
+    }
+
+    private class Adapter extends ArrayAdapter<Favorite> {
+        public Adapter(Context context, List<Favorite> favorites) {
+            super(context, 0, favorites);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Favorite favorite = getItem(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.view_double_line_emoticon, parent, false);
+            }
+            TextView emoticon = (TextView) convertView.findViewById(R.id.emoticonTextView);
+            TextView description = (TextView) convertView.findViewById(R.id.descriptionTextView);
+            emoticon.setText(favorite.getEmoticon());
+            description.setText(favorite.getDescription());
+            emoticon.setTextColor(getResources().getColor(android.R.color.black));
+            description.setTextColor(getResources().getColor(android.R.color.black));
+            return convertView;
         }
     }
 }
