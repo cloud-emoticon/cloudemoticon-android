786a630eaf85b5d05f5414e168cd5786dd06e553
KTachibanaM <whj19931115@gmail.com>
2019-07-26 17:41:29 -0700

    use importance for notification channels instead of priority

--- app/src/main/java/org/ktachibana/cloudemoji/utils/NotificationUtils.java
+++ app/src/main/java/org/ktachibana/cloudemoji/utils/NotificationUtils.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import org.ktachibana.cloudemoji.Constants;
 import org.ktachibana.cloudemoji.R;
@@ -34,13 +35,13 @@ public class NotificationUtils {
 
         // If only shows in panel
         else if (notificationVisibility.equals("panel")) {
-            createQuickTriggerNotificationChannel(context, notificationManager, NotificationCompat.PRIORITY_MIN);
+            createQuickTriggerNotificationChannel(context, notificationManager, NotificationManagerCompat.IMPORTANCE_MIN);
             showQuickTriggerNotification(context, notificationManager, NotificationCompat.PRIORITY_MIN);
         }
 
         // If shows on both panel and status bar
         else if (notificationVisibility.equals("both")) {
-            createQuickTriggerNotificationChannel(context, notificationManager, NotificationCompat.PRIORITY_DEFAULT);
+            createQuickTriggerNotificationChannel(context, notificationManager, NotificationManagerCompat.IMPORTANCE_DEFAULT);
             showQuickTriggerNotification(context, notificationManager, NotificationCompat.PRIORITY_DEFAULT);
         }
     }
@@ -48,13 +49,13 @@ public class NotificationUtils {
     private static void createQuickTriggerNotificationChannel(
             Context context,
             NotificationManager notificationManager,
-            int priority
+            int importance
     ) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                     Constants.QUICK_TRIGGER_NOTIFICATION_CHANNEL_ID,
                     context.getString(R.string.quick_trigger_notification_channel_name),
-                    priority
+                    importance
             );
             channel.setDescription(context.getString(R.string.quick_trigger_notification_channel_description));
             notificationManager.createNotificationChannel(channel);
