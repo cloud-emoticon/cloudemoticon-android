cc382eec5a6f802774103e9adef60c5cae08e9a8
KTachibanaM <whj19931115@gmail.com>
2020-05-31 13:51:07 -0700

    hmmm

--- app/src/main/java/org/ktachibana/cloudemoji/ime/MyInputMethodService.java
+++ app/src/main/java/org/ktachibana/cloudemoji/ime/MyInputMethodService.java
@@ -1,6 +1,11 @@
 package org.ktachibana.cloudemoji.ime;
 
 import android.inputmethodservice.InputMethodService;
+import android.view.View;
+
 
 public class MyInputMethodService extends InputMethodService {
+    @Override
+    public View onCreateInputView() {
+    }
 }
