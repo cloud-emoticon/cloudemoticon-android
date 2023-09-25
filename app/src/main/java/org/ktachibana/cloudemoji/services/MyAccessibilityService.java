40876add1aa4067cd8d50907a8c1787ad7dd6f43
KTachibanaM <whj19931115@gmail.com>
2023-09-25 16:28:47 -0700

    read fav and replace texts

--- app/src/main/java/org/ktachibana/cloudemoji/services/MyAccessibilityService.java
+++ app/src/main/java/org/ktachibana/cloudemoji/services/MyAccessibilityService.java
@@ -9,9 +9,12 @@ import android.view.accessibility.AccessibilityNodeInfo;
 
 import androidx.annotation.RequiresApi;
 
+import org.ktachibana.cloudemoji.models.disk.Favorite;
 import org.ktachibana.cloudemoji.utils.CapabilityUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MyAccessibilityService extends AccessibilityService {
     @Override
@@ -41,10 +44,28 @@ public class MyAccessibilityService extends AccessibilityService {
         if (text == null || text.length() == 0) {
             return;
         }
-        Log.d("MyAccessibilityService", "Retrieved text: " + text);
-        if (":kt".contentEquals(text)) {
+        for (final Favorite favorite: Favorite.listAll(Favorite.class)) {
+            final String shortcut = favorite.getShortcut();
+            if (shortcut == null || shortcut.length() == 0) {
+                continue;
+            }
+            final Pattern pattern = Pattern.compile(String.format(
+                    "^:%s| :%s",
+                    shortcut,
+                    shortcut));
+            final Matcher matcher = pattern.matcher(text);
+            if (!matcher.find()) {
+                continue;
+            }
+            final boolean isStart = matcher.start() == 0;
+            String replacedText;
+            if (isStart) {
+                replacedText = matcher.replaceFirst(favorite.getEmoticon());
+            } else {
+                replacedText = matcher.replaceFirst(" " + favorite.getEmoticon());
+            }
             Bundle arguments = new Bundle();
-            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, "ლ(╹◡╹ლ)");
+            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, replacedText);
             nodeInfo.performAction(
                     AccessibilityNodeInfo.AccessibilityAction.ACTION_SET_TEXT.getId(),
                     arguments);
